7a1fee39c2229ba43de35c0b8096235d2d1e8eb3
namassist <chairulanmm@gmail.com>
2023-05-17 01:27:24 +0700

    initial setup project with tailwind, redux-toolkit, react router and axios

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="h-screen w-full flex justify-center items-center bg-gray-800">
+      <h1 className="text-red-200 text-3xl">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
