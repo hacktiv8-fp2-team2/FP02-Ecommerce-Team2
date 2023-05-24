603f21da24334d52d6a4649fa0e071716faa45b5
namassist <chairulanmm@gmail.com>
2023-05-25 02:45:41 +0700

    add detail product

--- src/components/Templates/ProductDetail.jsx
+++ src/components/Templates/ProductDetail.jsx
@@ -1,47 +1,41 @@
-import Button from "../Atoms/Button";
 import { BackButton } from "../Atoms/BackButton";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getAllProducts } from "../../features/products/productSlice";
+import Button from "../Atoms/Button";
 import stars from "../../assets/images/stars.png";
 import bus from "../../assets/images/bus.png";
 import vector from "../../assets/images/Vector.png";
 
 export const ProductDetail = () => {
+  const { id } = useParams();
+  const products = useSelector(getAllProducts);
+  const product = products.find((p) => p.id === Number(id));
+
   return (
     <section className="container flex-1 ml-14 mt-24 mb-11">
       <BackButton />
       <div className="flex flex-row">
         <img
-          src="http://3.bp.blogspot.com/-Fq5YO2DpkMg/Uykx23LCjVI/AAAAAAAAAPc/Hkpdyt13bOI/s1600/cara+membuat+kopi.jpg"
+          src={product?.image}
           alt="Product Detail Picture"
           className="w-[35em] h-[29em] shadow-[3px_8px_12px_rgba(0,0,0,0.25)]"
         ></img>
         <div className="flex flex-col ml-14">
-          <h1 className="font-bold text-4xl mt-1 w-[14em]">Coffee Bundle</h1>
+          <h1 className="font-bold text-4xl mt-1 w-[14em]">{product?.title}</h1>
           <div className="flex flex-row items-end">
-            <img src={stars} className="w-[5em] h-[1.2em] mt-2 ml-1"></img>
-            <h1 className="font-bold text-sm ml-3">246 reviews</h1>
+            <h1 className="font-bold text-sm mt-1">
+              {product?.category} | {product?.rating?.count} reviews
+            </h1>
           </div>
           <p className="font-extralight text-justify mt-12">
-            Traditionally, bundles include a range of different products from
-            one brand. These items are then packaged and sold together. From a
-            consumer’s perspective, bundles allow them to purchase a ready-made
-            gift at a fraction of the time, and often, at a lower cost
+            {product?.description}
           </p>
-          <h3 className="font-bold mt-7 mb-2">Size</h3>
-          <div className="flex flex-row space-x-1">
-            <Button type={"button"} buttonSecondary>
-              Small
-            </Button>
-            <Button type={"button"} buttonPrimary>
-              Medium
-            </Button>
-            <Button type={"button"} buttonSecondary>
-              Big
-            </Button>
-          </div>
+
           <hr className="my-4"></hr>
           <div className="flex flex-row justify-between">
             <p className="text-xl">
-              Rp <span className="font-extrabold">2.500</span> / qty
+              $ <span className="font-extrabold">{product?.price}</span> / qty
             </p>
             <div className="flex flex-row space-x-3">
               <form>
@@ -49,7 +43,7 @@ export const ProductDetail = () => {
                   type="number"
                   min={1}
                   placeholder="1"
-                  className="w-[3.5em] text-center border border-neutral-900 rounded-md p-3"
+                  className="w-[4em] text-center border border-neutral-900 rounded-md p-3"
                 />
               </form>
               <Button type={"button"} buttonPrimary>
