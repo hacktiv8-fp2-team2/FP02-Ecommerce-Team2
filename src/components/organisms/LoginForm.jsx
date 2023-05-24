1ebb96f3c395a7aef96b2812f1b4fb8c6d540514
namassist <chairulanmm@gmail.com>
2023-05-24 22:56:36 +0700

    fix navbar

--- src/components/organisms/LoginForm.jsx
+++ src/components/organisms/LoginForm.jsx
@@ -16,6 +16,7 @@ const LoginForm = () => {
     let name = event.target.name;
     setInput({ ...input, [name]: value });
   };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     let { username, password } = input;
@@ -43,64 +44,68 @@ const LoginForm = () => {
   return (
     <>
       <div className="flex items-center justify-center min-h-screen bg-gray-100 mt-20">
-      <div className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0"
-      >
-        {/* <!-- left side --> */}
-        <div className="flex flex-col justify-center p-8 md:p-14">
-          <span className="mb-3 text-4xl font-bold">Welcome back</span>
-          <span className="font-light text-gray-400 mb-8">
-            Don’t have a account, Sign up
-          </span>
-          <form className="card-body" onSubmit={handleSubmit}>
-          <div className="py-4">
-            <span className="mb-2 text-md">Username</span>
-            <input
-              value={input.username}
-              onChange={handleInput}
-              type={"text"}
-              name="username"
-              className="input input-bordered"
-              placeholder="Email address"
-              style={{ width: "100%" }}
+        <div className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0">
+          {/* <!-- left side --> */}
+          <div className="flex flex-col justify-center p-8 md:p-14">
+            <span className="mb-3 text-4xl font-bold">Welcome back</span>
+            <span className="font-light text-gray-400 mb-8">
+              Don’t have a account, Sign up
+            </span>
+            <form className="card-body" onSubmit={handleSubmit}>
+              <div className="py-4">
+                <span className="mb-2 text-md">Username</span>
+                <input
+                  value={input.username}
+                  onChange={handleInput}
+                  type={"text"}
+                  name="username"
+                  className="input input-bordered"
+                  placeholder="Email address"
+                  style={{ width: "100%" }}
+                />
+              </div>
+              <div className="py-4">
+                <span className="mb-2 text-md">Password</span>
+                <input
+                  value={input.password}
+                  onChange={handleInput}
+                  type={"password"}
+                  name="password"
+                  className="input input-bordered"
+                  placeholder="Password"
+                  style={{ width: "100%" }}
+                />
+              </div>
+              <div className="flex justify-between w-full py-4">
+                <div className="mr-24">
+                  <input type="checkbox" name="ch" id="ch" className="mr-2" />
+                  <span className="text-md">Remember me</span>
+                </div>
+                <span
+                  className="font-bold text-md "
+                  style={{ color: "#3D85C6" }}
+                >
+                  Forgot password
+                </span>
+              </div>
+              <Button type={"submit"} buttonPrimary isFullWidth>
+                Login
+              </Button>
+            </form>
+          </div>
+
+          {/* <!-- right side --> */}
+          <div className="relative">
+            <img
+              className="py-8 "
+              src={loginpage}
+              alt="img"
+              width={675.54}
+              height={454.17}
             />
           </div>
-          <div className="py-4">
-            <span className="mb-2 text-md">Password</span>
-            <input
-              value={input.password}
-              onChange={handleInput}
-              type={"password"}
-              name="password"
-              className="input input-bordered"
-              placeholder="Password"
-              style={{ width: "100%" }}
-            />
-          </div>
-          <div className="flex justify-between w-full py-4">
-            <div className="mr-24">
-              <input type="checkbox" name="ch" id="ch" className="mr-2" />
-              <span className="text-md">Remember me</span>
-            </div>
-            <span className="font-bold text-md "style={{ color: "#3D85C6" }}>Forgot password</span>
-          </div>
-          <Button type={"submit"} buttonPrimary isFullWidth>
-            Login
-          </Button>
-          </form>
-        </div>
-        
-        {/* <!-- right side --> */}
-        <div className="relative">
-          <img
-            className="py-8 "
-            src={loginpage}
-            alt="img"
-            width={675.54}
-            height={454.17}
-          />
         </div>
       </div>
-    </div>
     </>
   );
 };
