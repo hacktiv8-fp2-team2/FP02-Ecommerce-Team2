6f82417dc15e275d946a7b425579bc8a6facb505
HP <202410102013@mail.unej.ac.id>
2023-05-26 01:05:37 +0700

    login to cart

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,8 +4,16 @@ export default function ProtectedRoute({ children }) {
     let location = useLocation();
 
     if(!localStorage.getItem("token")){
-        return <Navigate to="/login" state={{ from: location }} />
+        return <Navigate to="/" state={{ from: location }} />
     }
 
     return children;
 }
+// import { useSelector } from "react-redux";
+// import { Navigate, Outlet } from "react-router-dom";
+
+// const ProtectedRouteLogin = () => {
+//   const { login } = useSelector((state) => state.product);
+//   return JSON.parse(!localStorage.getItem("login")) || login === null ? <Outlet /> : <Navigate to="/" />;
+// };
+// export default ProtectedRouteLogin;
